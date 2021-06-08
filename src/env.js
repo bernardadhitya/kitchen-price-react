814795357ababbf9c06a10cee6cee90179448618
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-06-08 16:18:56 +0700

    [Bernard] Install supabase to dependencies

--- src/env.js
+++ src/env.js
@@ -7,4 +7,9 @@ const firebaseConfig = {
   appId: "1:969863311330:web:f7b1d3bb204f35104353bb"
 }
 
-export { firebaseConfig };
+const supabaseConfig = {
+  url: 'https://qjukedyogppqdaqyvbic.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjIzMTM1MTAzLCJleHAiOjE5Mzg3MTExMDN9.47AxP5lb7ijpaC6p006psBBv8NmYdb2jYmM7Zj8c35U'
+}
+
+export { firebaseConfig, supabaseConfig };
