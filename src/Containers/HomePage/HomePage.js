d3dbe4e3e63f662dc998adf2bc85963ac2984711
Bernard Adhitya <bernard.adhitya@gmail.com>
2021-06-13 15:33:10 +0700

    [Bernard] Add news row in home

--- src/Containers/HomePage/HomePage.js
+++ src/Containers/HomePage/HomePage.js
@@ -1,7 +1,28 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
+
+  const renderNewsCard = () => {
+    return (
+      <Grid item xs={3}>
+        <div className='news-card'>
+          <div className='news-image'>
+            <img
+              src={require('../../Assets/images/logo-bw.png')}
+              className={'news-thumbnail-empty'}
+              alt=''
+            />
+          </div>
+          <div className='news-content'>
+            <div className='news-title'>Lorem Ipsum</div>
+            <p>Ex duis laboris excepteur duis labore ipsum id commodo et consequat ad Lorem id dolore. Nostrud ipsum ut consequat duis nostrud eiusmod Lorem non exercitation adipisicing proident adipisicing proident. Labore reprehenderit qui exercitation duis duis nisi nulla occaecat pariatur cillum. Nostrud ullamco duis reprehenderit in qui amet qui.</p>
+          </div>
+        </div>
+      </Grid>
+    )
+  }
   
   return (
     <div className='home-page-wrapper'>
@@ -21,6 +42,15 @@ const HomePage = () => {
         <div className='home-redirect-button'>
           <p>Cari Sekarang</p>
         </div>
+      </div>
+      <div style={{textAlign: 'center', margin: '40px'}}>
+        <h1>Berita dan Artikel</h1>
+        <Grid container>
+          {renderNewsCard()}
+          {renderNewsCard()}
+          {renderNewsCard()}
+          {renderNewsCard()}
+        </Grid>
       </div>
     </div>
   )
